7b35fcf40f16ab8096127e2e499fbb689d7f2b85
Adam J. Smith <adam.smith733@my.simpson.edu>
2018-10-23 09:01:04 -0500

    added php common files, changed all html to php

---
+++ public_html/nav_bar.php
@@ -0,0 +1,43 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <img class="background-image" src="img/logo.png" alt="theater_logo">
+      <a class="navbar-brand" href="#">Simpson Movie Theater</a>
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="about.php">About</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" href="purchase_page.php">Buy Tickets</a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Movies
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="space_ghosts.php">Space Ghosts</a>
+              <a class="dropdown-item" href="comedy_movie.php">Comedy Movie</a>
+              <a class="dropdown-item" href="chef_dog.php">Chef Dog</a>
+              <a class="dropdown-item" href="action_movie.php">Action Movie</a>
+              <a class="dropdown-item" href="love_birds.php">Love Birds</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="ibook_movie.php">I-Book</a>
+
+            </div>
+          </li>
+          
+        </ul>
+      </div>
+    </nav>
